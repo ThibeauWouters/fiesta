9228feea1d4da2d0baebb6dfdac56d9cafa3a23c
ThibeauWouters <thibeauwouters@gmail.com>
2024-07-30 20:27:03 +0200

    first lines; pushing to Potsdam machines for Bulla LCs

---
+++ src/fiesta/base.py
@@ -0,0 +1,15 @@
+import flax
+
+class EMSurrogate:
+    
+    def __init__(self):
+        
+        pass
+    
+    def fit(self, X, y):
+        
+        pass
+    
+    def predict(self, X):
+        pass
+        
---
+++ src/fiesta/train.py
@@ -0,0 +1,9 @@
+"""Method to train the surrogate models"""
+
+class SurrogateTrainer:
+    
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
